56429e28c4247199b1e8f59653b683775d085067
Anton Momot <kolhospnuk@gmail.com>
2020-07-06 17:31:46 +0300

    -aaddForms

---
+++ src/components/header/header.js
@@ -0,0 +1,14 @@
+
+import React from "react";
+import './app-header.css';
+
+const AppHeader = () => {
+    return (
+        <div className='app-header d-flex'>
+            <h1>Momot Anton</h1>
+            <h2>5 записей, из ни понравилось 0</h2>
+        </div>
+    )
+}
+
+export default AppHeader;
